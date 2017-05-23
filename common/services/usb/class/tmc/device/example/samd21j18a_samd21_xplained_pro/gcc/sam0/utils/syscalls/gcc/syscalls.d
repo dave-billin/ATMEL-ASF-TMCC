9e8e0e3d71e4232fd531b77d251931480e0c2364
bill5510 <david.billin@vandals.uidaho.edu>
2017-05-22 22:20:36 -0700

    Add a copy of vendor sources as TMC class support

---
+++ common/services/usb/class/tmc/device/example/samd21j18a_samd21_xplained_pro/gcc/sam0/utils/syscalls/gcc/syscalls.d
@@ -0,0 +1,98 @@
+sam0/utils/syscalls/gcc/syscalls.o: \
+ ../../../../../../../../../sam0/utils/syscalls/gcc/syscalls.c \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/stdio.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/_ansi.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/newlib.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/_newlib_version.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/config.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/machine/ieeefp.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/features.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/cdefs.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/machine/_default_types.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/lib/gcc/arm-none-eabi/6.3.1/include/stddef.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/lib/gcc/arm-none-eabi/6.3.1/include/stdarg.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/reent.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/_ansi.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/_types.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/machine/_types.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/lock.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/types.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/_stdint.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/machine/endian.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/machine/_endian.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/select.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/_sigset.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/_timeval.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/timespec.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/_timespec.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/_pthreadtypes.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/machine/types.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/stdio.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/stat.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/time.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/machine/time.h \
+ /home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/_locale.h
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/stdio.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/_ansi.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/newlib.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/_newlib_version.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/config.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/machine/ieeefp.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/features.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/cdefs.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/machine/_default_types.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/lib/gcc/arm-none-eabi/6.3.1/include/stddef.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/lib/gcc/arm-none-eabi/6.3.1/include/stdarg.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/reent.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/_ansi.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/_types.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/machine/_types.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/lock.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/types.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/_stdint.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/machine/endian.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/machine/_endian.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/select.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/_sigset.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/_timeval.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/timespec.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/_timespec.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/_pthreadtypes.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/machine/types.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/stdio.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/stat.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/time.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/machine/time.h:
+
+/home/dave/opt/gcc-arm-none-eabi-6-2017-q1-update/arm-none-eabi/include/sys/_locale.h:
